0f631b03f0fec30505ae675ca24e734dea36b9e2
Sana Rzgar <sanarizgar34@gmail.com>
2022-08-22 22:16:31 +0300

    fix an issue

--- db.js
+++ db.js
@@ -1,11 +1,5 @@
-const items = require("./data/items");
-const tags = require("./data/tags");
-const brands = require("./data/brands");
-const itemTypes = require("./data/itemTypes");
+const hotelsData = require("./data/hotelsData.json");
 
 module.exports = {
-  items,
-  tags,
-  brands,
-  itemTypes,
+  hotelsData,
 };
